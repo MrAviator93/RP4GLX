e2c7ce3ff05e532ad9b19c0fceab991599200104
MrAviator93 <kalninsarturs@hotmail.com>
2021-05-15 09:46:52 +0100

    Initial commit

---
+++ WindowX11.hpp
@@ -0,0 +1,54 @@
+/***********************************************************************
+FILE WINDOWX11.HPP CONTAINS LINUX OPERATING SYSTEM WINDOW IMPLEMENTATION
+
+FILE UTMOST REVIEW DONE ON (24.01.2021) BY ARTUR K. 
+***********************************************************************/
+
+#ifndef BENGINE_WINDOW_X11_HPP__
+#define BENGINE_WINDOW_X11_HPP__
+
+#include <cstdint>
+
+struct _XDisplay;
+
+namespace BEngine 
+{
+    //-----------------------------------------------------------------------
+	// LINUX X11 WINDOW WRAPPER CLASS
+	//
+	// Windows window class wrapper, uses X11 library for implementation.
+	//-----------------------------------------------------------------------
+
+	class CWindowX11 
+    {
+        friend class CX11MessageCallback;
+    public:
+        CWindowX11();
+        ~CWindowX11();
+
+        void update();
+
+        std::uint32_t clientWidth() const;
+        std::uint32_t clientHeight() const;
+
+        bool running() const 
+        {
+            return m_running;
+        }
+
+        void* getWindowHandle() const
+        {
+            return m_pWindowHandle;
+        }
+
+    private:
+        void createWindow() noexcept ( false );
+
+        _XDisplay* pDisplay{ nullptr };
+        void* m_pWindowHandle { nullptr }; // Window
+        bool m_running { false };
+        std::uint64_t wmDeleteMessage;
+    };
+
+} // namespace BEngine 
+#endif // BENGINE_WINDOW_X11_HPP__
---
+++ main.cpp
@@ -0,0 +1,73 @@
+/***********************************************************************
+FILE MAIN.CPP CONTAINS EXPERIMENTAL CODE TO CREATE X11 WINDOW
+
+http://manpages.ubuntu.com/manpages/bionic/en/man3/XCreateWindow.3.html
+https://jan.newmarch.name/LinuxSound/Diversions/RaspberryPiOpenGL/
+https://wiki.maemo.org/SimpleGL_example
+
+
+1. Install the following libs: 
+sudo apt-get install libx11-dev
+sudo apt-get install libglew-dev - not needed !!!
+sudo apt-get install mesa-utils
+sudo apt install libglm-dev
+
+2. Don't forget to link the following libs: 
+-lX11 -lGL -lGLEW
+
+FILE UTMOST REVIEW DONE ON (23.01.2021) BY ARTUR K. 
+***********************************************************************/
+
+#include "WindowX11.hpp"
+#include "GLXDevice.hpp"
+
+#include <fmt/format.h>
+#include <date/date.h>
+
+// Window related libs
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <X11/keysymdef.h>
+#include <X11/Xatom.h>
+
+// OpenGL related libs
+#include <EGL/egl.h>
+#include <EGL/eglext.h>
+#include <GLES3/gl3.h>
+
+// Maths libraries
+#include <glm/vec3.hpp>
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <thread>
+#include <chrono>
+#include <stdio.h>
+#include <math.h>
+
+// There is an issue with fmt/format (fmtlib)
+// https://github.com/fmtlib/fmt/issues/795
+
+int main( int argc, char* argv[] )
+{
+    fmt::print( "Welcome to Raspbery PI 4 OpenGL!\n" );
+
+    BEngine::CWindowX11 window;
+
+    // Set-up OpenGL ES
+    BEngine::Graphics::CGLXDevice glxDevice{ window.getWindowHandle() };
+    glxDevice.viewport( 0, 0, 1200, 800 );
+    glxDevice.clearColourDepthStencil();
+    glxDevice.clearColour( 0.24, 0.14, 0.24, 1.0f );
+
+    while ( window.running() )
+    {
+        window.update();
+        glxDevice.clearColourDepthStencil();
+        glxDevice.clearColour( 0.24, 0.14, 0.24, 1.0f );
+        glxDevice.swapBuffers();
+    }
+
+    return 0;
+}
